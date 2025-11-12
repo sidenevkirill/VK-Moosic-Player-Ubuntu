63b6ca59864fd275eb7ccd4f1fd7a81e3b09006c
Сиденёв Кирилл <wwwkom780@gmail.com>
2025-11-12 20:16:06 +0000

    Update install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -7,7 +7,11 @@ sudo apt update
 # Установка системных зависимостей
 sudo apt install -y python3-gi python3-gi-cairo gir1.2-gtk-3.0 python3-requests mplayer
 
+# Установите python-dotenv
+pip3 install --user python-dotenv
+
 # Установка Python зависимостей через pip
 pip3 install --user python-dotenv requests
 
 echo "Зависимости установлены!"
+
